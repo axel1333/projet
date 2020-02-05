009ff36975cbe96e12b353ca187bcfcb021c9ff4
axel13333 <axel566@hotmail.fr>
2020-02-05 14:43:57 +0100

    php

---
+++ jour6/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <header>
+
+            <h1> La meilleure page !!  </h1>
+
+    <nav>
+            <a href="index.php">accueil</a>
+            <a href="services.php">services</a>
+            <a href="contact.php">contact</a>
+
+    </nav>
+
+    </header>
+
+
+
+<main>
+
+    <section> 
+
+    <h2> Tinquiete pas le site va devenir unique , ce n'est qu'une question de temps </h2>
+
+    <p> Deuxième ligne du meilleure site  </p>
+
+
+    </section>
+
+
+
+
+
+</main>
+
+
+
+    <footer>  Tout droit réservé A.B  </footer>
+
+
+
+    
+</body>
+
+</html>
---
+++ jour6/php/view/footer.php
@@ -0,0 +1,7 @@
+ <!-- DECOUPAGE EN PHP -->
+ </main>
+    <footer>
+        <p>tous droits réservés</p>
+    </footer>
+</body>
+</html>
